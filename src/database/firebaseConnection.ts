c239932c4103919a437d3b39b2d704135d205bc6
Joao010 <joaopedrocz12345@gmail.com>
2022-03-27 22:36:15 -0300

    responsive(header): add responsive design to header

--- src/database/firebaseConnection.ts
+++ src/database/firebaseConnection.ts
@@ -2,14 +2,8 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-let firebaseConfig = {
-	apiKey: "AIzaSyBkEe4N3XG9nqcXzOnx9mFP8DZx_p9yTYQ",
-	authDomain: "pomodoro-webapp.firebaseapp.com",
-	projectId: "pomodoro-webapp",
-	storageBucket: "pomodoro-webapp.appspot.com",
-	messagingSenderId: "940628448117",
-	appId: "1:940628448117:web:a2440b2f4341090123b93c",
-	measurementId: "G-NC405DQJTS",
+const firebaseConfig = {
+	/* ** configs ** */
 };
 
 if (!firebase.apps.length) {
